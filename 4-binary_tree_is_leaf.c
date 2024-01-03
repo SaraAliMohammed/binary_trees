cfede78efb069ff3801bec18ffe697b246ad6b9e
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-01-03 12:44:08 -0800

    4. Is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,6 +10,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node || node->left || node->right)
 		return (0);
-	if (!node->left && !node->right)
+	if (!(node->left) && !(node->right))
 		return (1);
 }
