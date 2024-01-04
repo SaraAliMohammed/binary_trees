0e4b1d0b6437bb161228f30a6eb7b7cb4db4a6cf
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-01-04 01:42:19 -0800

    12. Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,10 +12,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left || tree->right)
+	if (!(tree->left) || !(tree->right))
 		count = 1;
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
+	count += binary_tree_leaves(tree->left);
+	count += binary_tree_leaves(tree->right);
 	
 	return (count);
 }
