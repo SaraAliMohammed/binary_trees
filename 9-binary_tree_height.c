a20b561a18e13b5111f0bb9289cd1b6b2cd70830
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-01-04 00:38:31 -0800

    9. Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to traverse.
+ *
+ * Return: Nothing
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
